3aa3dd86bfc26b904438b8011d22c771589c929e
Zhenyun Yu <me@yuzhenyun.me>
2020-07-17 22:54:53 +0800

    upgrade pkgs.

---
+++ elpa/utop-2.6.0/utop-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "utop" "2.6.0" "Universal toplevel for OCaml" '((emacs "24")) :commit "df2447a63de2fea0f56d8c63d35b0bf39e11c0f2" :keywords '("ocaml" "languages") :authors '(("Jeremie Dimino" . "jeremie@dimino.org")) :maintainer '("Jeremie Dimino" . "jeremie@dimino.org") :url "https://github.com/diml/utop")
