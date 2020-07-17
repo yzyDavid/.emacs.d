6e7bbc6a5764f7d6d06ce95d228bad2f5917e2d8
Zhenyun Yu <me@yuzhenyun.me>
2020-07-18 00:22:11 +0800

    plugins for scala.

---
+++ elpa/bui-1.2.1/bui-pkg.el
@@ -0,0 +1,13 @@
+(define-package "bui" "1.2.1" "Buffer interface library"
+  '((emacs "24.3")
+    (dash "2.11.0"))
+  :commit "9162c24b75799857d54838d961c60776ffcd657e" :keywords
+  '("tools")
+  :authors
+  '(("Alex Kost" . "alezost@gmail.com"))
+  :maintainer
+  '("Alex Kost" . "alezost@gmail.com")
+  :url "https://github.com/alezost/bui.el")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ elpa/lsp-metals-1.1.0/lsp-metals-pkg.el
@@ -0,0 +1,20 @@
+(define-package "lsp-metals" "1.1.0" "Scala Client settings"
+  '((emacs "26.1")
+    (lsp-mode "7.0")
+    (dap-mode "0.3")
+    (dash "2.14.1")
+    (dash-functional "2.14.1")
+    (f "0.20.0")
+    (ht "2.0")
+    (treemacs "2.5"))
+  :commit "efefcc0e936ec463f0d19b6cae7c8336dcd186e4" :keywords
+  '("languages" "extensions")
+  :authors
+  '(("Ross A. Baker" . "ross@rossabaker.com")
+    ("Evgeny Kurnevsky" . "kurnevsky@gmail.com"))
+  :maintainer
+  '("Ross A. Baker" . "ross@rossabaker.com")
+  :url "https://github.com/emacs-lsp/lsp-metals")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
