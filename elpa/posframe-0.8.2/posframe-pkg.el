a819377cf11b2bbdf15f8b53ca0e9511455b0f41
Zhenyun Yu <me@yuzhenyun.me>
2020-11-18 11:54:10 +0800

    update

---
+++ elpa/posframe-0.8.2/posframe-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "posframe" "0.8.2" "Pop a posframe (just a frame) at point" '((emacs "26")) :commit "395aca928b00c8f76aaeb65a85481c99e88c6873" :keywords '("convenience" "tooltip") :authors '(("Feng Shu" . "tumashu@163.com")) :maintainer '("Feng Shu" . "tumashu@163.com") :url "https://github.com/tumashu/posframe")
