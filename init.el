6213dd2996a3ae9515cce9358eb603996078fbbf
Zhenyun Yu <me@yuzhenyun.me>
2020-12-12 23:42:08 +0800

    u

--- init.el
+++ init.el
@@ -45,7 +45,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(sbt-mode lsp-metals lsp-treemacs magit-popup markdown-mode treemacs ess evil go-mode lsp-mode lsp-ui rust-mode scala-mode tuareg use-package utop web-mode yaml-mode magit ccls bazel-mode ace-window dockerfile-mode flymake company-lsp gnu-elpa-keyring-update)))
+   '(po-mode sbt-mode lsp-metals lsp-treemacs magit-popup markdown-mode treemacs ess evil go-mode lsp-mode lsp-ui rust-mode scala-mode tuareg use-package utop web-mode yaml-mode magit ccls bazel-mode ace-window dockerfile-mode flymake company-lsp gnu-elpa-keyring-update)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
